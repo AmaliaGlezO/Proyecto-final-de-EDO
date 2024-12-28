fb66447fd45ae2ac05e12f35c1a0119ffb4ca1ae
AmaliaGlezO <amalia.glezo@gmail.com>
2024-12-28 12:35:38 -0500

    Add readme

--- SED.py
+++ SED.py
@@ -1,2 +1,27 @@
-# Soluciones de los SED
-print("hola mundo")
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+# Definir el sistema de ecuaciones diferenciales
+def sistema(y, t):
+    x, z = y
+    dxdt = z
+    dzdt = -x
+    return [dxdt, dzdt]
+
+# Condiciones iniciales
+y0 = [1.0, 0.0]
+
+# Puntos de tiempo donde se evaluará la solución
+t = np.linspace(0, 10, 100)
+
+# Resolver el sistema de ecuaciones diferenciales
+sol = odeint(sistema, y0, t)
+
+# Graficar los resultados
+plt.plot(t, sol[:, 0], label='x(t)')
+plt.plot(t, sol[:, 1], label='z(t)')
+plt.xlabel('Tiempo')
+plt.ylabel('Soluciones')
+plt.legend()
+plt.show()
